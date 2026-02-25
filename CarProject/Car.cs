cc6418296c17b8af2f9a419bb637158073832941
Ozgur <ozgurozvaris@hotmail.com>
2026-02-25 09:48:32 +0300

    Add project files.

---
+++ CarProject/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarProject
+{
+    public class Car
+    {
+        public string Brand { get; set; }
+        public string Color { get; set; }
+        public int Speed { get; set; }
+
+        public Car(string brand, string color)
+        {
+            Brand = brand;
+            Color = color;
+
+            // Console.WriteLine($"Car created: {Brand} in {Color}");
+            Console.WriteLine("Car created: " + Brand + " in " + Color);
+        }
+
+
+
+    }
+}
---
+++ CarProject/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace CarProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car("Toyota", "Red");
+
+            Console.ReadLine();
+        }
+    }
+}
